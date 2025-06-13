d50fc2ab097d3222bb1c7d31cfb72041ebb086a1
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2025-06-13 18:45:27 +0300

    реализовал translation outbox и обновил OrderControllers.cs

--- OrdersService/Controllers/OrderControllers.cs
+++ OrdersService/Controllers/OrderControllers.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using OrdersService.Data;
 using OrdersService.Interfaces;
 using OrdersService.Models;
 using OrdersService.Services;
@@ -10,38 +11,58 @@ namespace OrdersService.Controllers;
 [Route("orders")]
 public class OrderControllers : ControllerBase
 {
-    private readonly InMemoryOrderStore _orderStore;
+    // private readonly InMemoryOrderStore _orderStore;
     private readonly IPaymentPublisher _paymentPublisher;
-
-    public OrderControllers(InMemoryOrderStore orderStore, IPaymentPublisher fakePaymentPublisher)
+    private readonly OrdersDbContext _dbContext;
+    
+    public OrderControllers(OrdersDbContext dbContext, IPaymentPublisher publisher)
     {
-        _orderStore = orderStore;
-        _paymentPublisher = fakePaymentPublisher;
+        _dbContext = dbContext;
+        _paymentPublisher = publisher;
     }
 
     [HttpPost("create")]
-    public IActionResult CreateOrder([FromBody] OrderRequest request)
+    public async Task<ActionResult<Order>> CreateOrder([FromBody] OrderRequest request)
     {
-        var order = _orderStore.CreateOrder(request.CustomerId, request.Amount);
+        await using var tx = await _dbContext.Database.BeginTransactionAsync();
+
+        var order = new Order
+        {
+            CustomerId = request.CustomerId,
+            Amount = request.Amount,
+            OrderStatus = OrderStatus.PaymentPending
+        };
         
-        _ = Task.Run(() => _paymentPublisher.PublishOrderAsync(order));
+        _dbContext.Orders.Add(order);
+
+        var outbox = new OutboxMessage
+        {
+            EventType = "OrderCreated",
+            Payload = System.Text.Json.JsonSerializer.Serialize(order)
+        };
+        
+        _dbContext.OutboxMessages.Add(outbox);
+        
+        await _dbContext.SaveChangesAsync();
+        await tx.CommitAsync();
         
         return Ok(order);
+
     }
 
-    [HttpGet("user")]
-    public ActionResult<IEnumerable<Order>> GetOrdersForUser([FromQuery] Guid userId)
-    {
-        var orders = _orderStore.GetOrders(userId);
-        return Ok(orders);
-    }
-
-    [HttpGet("{orderId}")]
-    public ActionResult<Order> GetOrderStatus(Guid orderId)
-    {
-        var order = _orderStore.GetById(orderId);
-        if (order == null)
-            return NotFound("Order not found");
-        return Ok(order);
-    }
+    // [HttpGet("user")]
+    // public ActionResult<IEnumerable<Order>> GetOrdersForUser([FromQuery] Guid userId)
+    // {
+    //     var orders = _orderStore.GetOrders(userId);
+    //     return Ok(orders);
+    // }
+    //
+    // [HttpGet("{orderId}")]
+    // public ActionResult<Order> GetOrderStatus(Guid orderId)
+    // {
+    //     var order = _orderStore.GetById(orderId);
+    //     if (order == null)
+    //         return NotFound("Order not found");
+    //     return Ok(order);
+    // }
 }
